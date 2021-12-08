5af331030357fff763d2ab4faff13e2acac938e9
Aakash Adesara <aakash.adesara@gmail.com>
2021-12-07 19:11:23 -0800

    more updates

--- src/Navbar.js
+++ src/Navbar.js
@@ -16,25 +16,25 @@ export const Navbar = (props) => {
         AAKASH ADESARA
       </div>
       <div
-        onClick={() => (window.location = "/work")}
+        onClick={() => (window.location = "/#/work")}
         style={{ width: "15%", textAlign: "center", cursor: "pointer" }}
       >
         WORK
       </div>
       <div
-        onClick={() => (window.location = "/writing")}
+        onClick={() => (window.location = "/#/writing")}
         style={{ width: "15%", textAlign: "center", cursor: "pointer" }}
       >
         WRITING
       </div>
       <div
-        onClick={() => (window.location = "/art")}
+        onClick={() => (window.location = "/#/art")}
         style={{ width: "15%", textAlign: "center", cursor: "pointer" }}
       >
         ART
       </div>
       <div
-        onClick={() => (window.location = "/books")}
+        onClick={() => (window.location = "/#/books")}
         style={{ width: "15%", textAlign: "center", cursor: "pointer" }}
       >
         BOOKS
