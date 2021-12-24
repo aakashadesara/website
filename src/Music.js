c5cb93d045e8f16163b03b2692ae8032ee84bb6e
Aakash Adesara <aakash.adesara@gmail.com>
2021-12-24 00:34:57 -0800

    Fix Warning Messages

--- src/Music.js
+++ src/Music.js
@@ -1,29 +1,6 @@
 import "./App.css";
 import { Navbar } from "./Navbar";
-import AakashPortrait from "./assets/art/aakash.jpeg";
-import Mandalorian from "./assets/art/mandalorian.jpeg";
-import Bojack from "./assets/art/bojack.png";
-import Fish from "./assets/art/fish.png";
-import Japan from "./assets/art/japan.jpeg";
-import Owl from "./assets/art/owl.jpeg";
-import Lumber from "./assets/art/lumber.jpeg";
-import Eclipse from "./assets/art/eclipse.jpeg";
-
-import Punk1 from "./assets/punks/1.png";
-import Punk2 from "./assets/punks/2.png";
-import Punk3 from "./assets/punks/3.png";
-import Punk4 from "./assets/punks/4.png";
-import Punk5 from "./assets/punks/5.png";
-import Punk6 from "./assets/punks/6.png";
-import Punk7 from "./assets/punks/7.png";
-import Punk8 from "./assets/punks/8.png";
-import Punk9 from "./assets/punks/9.png";
-import Punk10 from "./assets/punks/10.png";
-
-import Bowser from "./assets/art/bowser.jpeg";
 import { Footer } from "./Footer";
-import { Pill } from "./Pill";
-import { HighlightPill } from "./HighlightPill";
 
 export function Music() {
   return (
@@ -33,6 +10,7 @@ export function Music() {
       <Navbar />
 
       <iframe
+        title="aakash song 1"
         width="100%"
         height="166"
         scrolling="no"
@@ -42,6 +20,7 @@ export function Music() {
       ></iframe>
 
       <iframe
+        title="aakash song 2"
         width="100%"
         height="166"
         scrolling="no"
