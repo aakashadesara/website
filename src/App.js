790f65274973762d191cdd2db5529f679330facb
Aakash Adesara <aakash.adesara@gmail.com>
2022-06-14 17:03:10 -0700

    website updates

--- src/App.js
+++ src/App.js
@@ -8,23 +8,32 @@ import { Work } from "./Work";
 import { Writing } from "./Writing";
 import { Music } from "./Music";
 
+import { Keysho } from "keysho";
+import "keysho/dist/index.css";
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
+    <>
+      <Keysho
+        config_uuid={"b46acd46-e92c-11ec-995a-1eab7e003dc6"}
+        actionMap={{}}
+      />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
 
-        <Route path="/work" element={<Work />} />
+          <Route path="/work" element={<Work />} />
 
-        <Route path="/writing" element={<Writing />} />
+          <Route path="/writing" element={<Writing />} />
 
-        <Route path="/music" element={<Music />} />
+          <Route path="/music" element={<Music />} />
 
-        <Route path="/art" element={<Art />} />
+          <Route path="/art" element={<Art />} />
 
-        <Route path="/books" element={<Books />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="/books" element={<Books />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
