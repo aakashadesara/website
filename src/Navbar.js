1944987cb41ffc95a530ae549fb9ddc9970cf1d4
Aakash Adesara <aakash.adesara@gmail.com>
2023-01-14 20:06:50 -0800

    Added Mantine and Dark Mode

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,50 +1,55 @@
 import { Link } from "react-router-dom";
+import { ActionToggle } from "./components/colorSchemeToggle";
 export const Navbar = (props) => {
   return (
-    <div
-      style={{
-        flexDirection: "row",
-        marginBottom: 24,
-        display: "flex",
-        fontSize: 12,
-        marginTop: 8,
-      }}
-    >
-      <Link to="/" style={{ width: "40%", cursor: "pointer" }}>
-        AAKASH ADESARA
-      </Link>
-      <Link
-        to="/work"
-        style={{ width: "20%", textAlign: "right", cursor: "pointer" }}
-      >
-        WORK
-      </Link>
-      <Link
-        to="/writing"
-        style={{ width: "20%", textAlign: "right", cursor: "pointer" }}
-      >
-        WRITING
-      </Link>
+    <div>
+      <div style={{ position: "absolute", top: 0, right: 20 }}>
+        <ActionToggle />
+      </div>
 
-      <Link
-        to="/music"
-        style={{ width: "16%", textAlign: "right", cursor: "pointer" }}
+      <div
+        style={{
+          flexDirection: "row",
+          marginBottom: 24,
+          display: "flex",
+          fontSize: 12,
+          marginTop: 60,
+        }}
       >
-        MUSIC
-      </Link>
-
-      <Link
-        to="/art"
-        style={{ width: "12%", textAlign: "right", cursor: "pointer" }}
-      >
-        ART
-      </Link>
-      <Link
-        to="/books"
-        style={{ width: "18%", textAlign: "right", cursor: "pointer" }}
-      >
-        BOOKS
-      </Link>
+        <Link to="/" style={{ width: "40%", cursor: "pointer" }}>
+          AAKASH ADESARA
+        </Link>
+        <Link
+          to="/work"
+          style={{ width: "20%", textAlign: "right", cursor: "pointer" }}
+        >
+          WORK
+        </Link>
+        <Link
+          to="/writing"
+          style={{ width: "20%", textAlign: "right", cursor: "pointer" }}
+        >
+          WRITING
+        </Link>
+        <Link
+          to="/music"
+          style={{ width: "16%", textAlign: "right", cursor: "pointer" }}
+        >
+          MUSIC
+        </Link>
+        <Link
+          to="/art"
+          style={{ width: "12%", textAlign: "right", cursor: "pointer" }}
+        >
+          ART
+        </Link>
+        <Link
+          to="/books"
+          style={{ width: "18%", textAlign: "right", cursor: "pointer" }}
+        >
+          BOOKS
+        </Link>{" "}
+      </div>
     </div>
   );
 };
