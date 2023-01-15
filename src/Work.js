7b47917f55846b187a60bcaf3e932df4960ceab5
Aakash Adesara <aakash.adesara@gmail.com>
2023-01-14 21:49:33 -0800

    Added Work Accordian

--- src/Work.js
+++ src/Work.js
@@ -7,58 +7,116 @@ import CalHacksLogo from "./assets/calhacks.jpeg";
 import LyftLogo from "./assets/lyft.jpeg";
 import NextdoorLogo from "./assets/nextdoor.png";
 import YelpLogo from "./assets/yelp.jpeg";
+import SellScaleLogo from "./assets/sellscale.png";
 
 import { WorkEntry } from "./WorkEntry";
 import { Footer } from "./Footer";
 
+import { Group, Avatar, Text, Accordion } from "@mantine/core";
+
+const charactersList = [
+  {
+    id: "SellScale",
+    image: SellScaleLogo,
+    label: "SellScale - Co-Founder",
+    description: "AI, Sales, Product, Engineering",
+    content:
+      "SellScale is a fast-growing venture backed AI startup. Our mission is to bring AI powered tools to sales teams to dramatically improve their sales outcomes and ultimately enable businesses to grow more efficiently.",
+  },
+  {
+    id: "athelas",
+    image: AthelasLogo,
+    label: "Athelas - Lead Growth Engineer",
+    description: "Product, Growth, and Engineering",
+    content:
+      "Athelas is a fast-growing Series-B stage healthcare startup. Our mission is to bring modern healthcare services to patients with chronic illnesses to dramatically improve their health outcomes and ultimately reduce the cost of care.",
+  },
+
+  {
+    id: "nextdoor",
+    image: NextdoorLogo,
+    label: "Nextdoor - Growth Engineer",
+    description: "Growth Virality, Consumer Product",
+    content:
+      "Nextdoor is the app where you plug into the neighborhoods that matter to you. I was involved in building out several of Nextdoor's organic network effects through the Growth Virality team. ($KIND)",
+  },
+
+  {
+    id: "calhacks",
+    image: CalHacksLogo,
+    label: "Cal Hacks - Executive Director",
+    description: "Hackathon, UC Berkeley",
+    content:
+      "Cal Hacks is the world's largest collegiate hackathon hosted annually at UC Berkeley. I was a director for 4 years and led the team for Cal Hacks 5.0 (2200+ attendees, kickstart of the Cal Hacks Fellowship startup incubator).",
+  },
+
+  {
+    id: "BTF",
+    image: BTFLogo,
+    label: "CS 198: Build the Future - Instructor",
+    description: "Entrepreneurship, Speaker Series",
+    content:
+      "CS 198 is Cal's entrepreneurial speaker series. We've brought in speakers like Steve Wozniak (Apple), Marshawn Lynch (Beast), Patrick Collison (Stripe) and more to dive into their entrepreneurial journeys in an intimate environment.",
+  },
+
+  {
+    id: "lyft",
+    image: LyftLogo,
+    label: "Lyft - ML SWE Internship",
+    description: "Machine Learning, Pricing",
+    content:
+      "I interned at Lyft on the Machine Learning division of the Adaptive Pricing team. We built models that found the optimal pricing across all nodes of Lyft's network to maximize revenue for the rideshare business.",
+  },
+  {
+    id: "Yelp",
+    image: YelpLogo,
+    label: "Yelp - Android Engineer Internship",
+    description: "Android Development",
+    content:
+      "Yelp had just acquired a reservation company and I was responsible for integrating with, and natively supporting the reservation capabilities in Yelp's main Android application.",
+  },
+];
+
+interface AccordionLabelProps {
+  label: string;
+  image: string;
+  description: string;
+}
+
+function AccordionLabel({ label, image, description }: AccordionLabelProps) {
+  return (
+    <Group noWrap>
+      <Avatar src={image} radius="xl" size="lg" />
+      <div>
+        <Text>{label}</Text>
+        <Text size="sm" color="dimmed" weight={400}>
+          {description}
+        </Text>
+      </div>
+    </Group>
+  );
+}
+
 export function Work() {
+  const items = charactersList.map((item) => (
+    <Accordion.Item value={item.id} key={item.label}>
+      <Accordion.Control>
+        <AccordionLabel {...item} />
+      </Accordion.Control>
+      <Accordion.Panel>
+        <Text size="sm">{item.content}</Text>
+      </Accordion.Panel>
+    </Accordion.Item>
+  ));
+
   return (
     <div
       style={{ maxWidth: 450, margin: "0% auto", padding: 16, fontSize: 14 }}
     >
       <Navbar />
-
-      <WorkEntry
-        logo={AthelasLogo}
-        title={"Athelas"}
-        tags={["Growth Engineer", "Healthcare"]}
-        description="Athelas is a fast-growing Series-B stage healthcare startup. Our mission is to bring modern healthcare services to patients with chronic illnesses to dramatically improve their health outcomes and ultimately reduce the cost of care. "
-      />
-
-      <WorkEntry
-        logo={NextdoorLogo}
-        title={"Nextdoor"}
-        tags={["Software Engineer", "Community"]}
-        description="Nextdoor is the app where you plug into the neighborhoods that matter to you. I was involved in building out several of Nextdoor's organic network effects through the Growth Virality team. ($KIND)"
-      />
-
-      <WorkEntry
-        logo={CalHacksLogo}
-        title={"Cal Hacks"}
-        tags={["Executive Director", "Hackathon"]}
-        description="Cal Hacks is the world's largest collegiate hackathon hosted annually at UC Berkeley. I was a director for 4 years and led the team for Cal Hacks 5.0 (2200+ attendees, kickstart of the Cal Hacks Fellowship startup incubator)."
-      />
-
-      <WorkEntry
-        logo={BTFLogo}
-        title={"CS 198: How to Build the Future"}
-        tags={["Course Instructor", "UC Berkeley"]}
-        description="CS 198 is Cal's entrepreneurial speaker series. We've brought in speakers like Steve Wozniak (Apple), Marshawn Lynch (Beast), Patrick Collison (Stripe) and more to dive into their entrepreneurial journeys in an intimate environment."
-      />
-
-      <WorkEntry
-        logo={LyftLogo}
-        title={"Lyft"}
-        tags={["Machine Learning"]}
-        description="I interned at Lyft on the Machine Learning division of the Adaptive Pricing team. We built models that found the optimal pricing across all nodes of Lyft's network to maximize revenue for the rideshare business."
-      />
-
-      <WorkEntry
-        logo={YelpLogo}
-        title={"Yelp"}
-        tags={["Android Development"]}
-        description="Yelp had just acquired a reservation company and I was responsible for integrating with, and natively supporting the reservation capabilities in Yelp's main Android application."
-      />
+      <Accordion chevronPosition="right" variant="contained">
+        {items}
+      </Accordion>
 
       <Footer />
     </div>
---
+++ src/components/Spotlight.js
@@ -0,0 +1,55 @@
+import { Button, Group } from "@mantine/core";
+import { openSpotlight } from "@mantine/spotlight";
+import type { SpotlightAction } from "@mantine/spotlight";
+import { IconHome, IconDashboard, IconFileText } from "@tabler/icons";
+import { TextInput } from "@mantine/core";
+import { IconCommand } from "@tabler/icons";
+
+export function SpotlightControl() {
+  return (
+    <Group position="center" style={{ width: "400px" }}>
+      <TextInput
+        placeholder="Search (or CMD + K)"
+        size="xs"
+        onClick={() => openSpotlight()}
+        icon={<IconCommand size={14} />}
+      />
+    </Group>
+  );
+}
+
+export const SPOTLIGHT_ACTIONS: SpotlightAction[] = [
+  {
+    title: "Home",
+    description: "Go to the homepage",
+    onTrigger: () => (window.location = "/"),
+    icon: <IconHome size={18} />,
+  },
+  {
+    title: "Work",
+    description: "Learn about my work",
+    onTrigger: () => (window.location = "/work"),
+    icon: <IconDashboard size={18} />,
+  },
+
+  {
+    title: "Writing",
+    description: "Read some of my writing",
+    onTrigger: () => (window.location = "/writing"),
+    icon: <IconDashboard size={18} />,
+  },
+
+  {
+    title: "Music",
+    description: "Listen to some of my music",
+    onTrigger: () => (window.location = "/music"),
+    icon: <IconDashboard size={18} />,
+  },
+
+  {
+    title: "Books",
+    description: "Read some of my books",
+    onTrigger: () => (window.location = "/books"),
+    icon: <IconDashboard size={18} />,
+  },
+];
